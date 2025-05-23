c711241a0d829614de90debdbf2945ec22871ddc
M Anees <manees@Ms-MacBook-Pro.local>
2025-05-23 19:33:09 +0500

    first commit

--- iconScount/expired/index.php
+++ iconScount/expired/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Session Expired - StealthWriter</title>
+    <title>Session Expired</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome for icons -->
